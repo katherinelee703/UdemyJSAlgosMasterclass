35ffcb5d04e3267ca3573edf11f1fe4623488b96
Katie Durand <khd25@cornell.edu>
2020-10-16 15:40:06 -0500

    add testing

--- additionalPracticeProblems/sll/singlyLinkedListRebuild.js
+++ additionalPracticeProblems/sll/singlyLinkedListRebuild.js
@@ -150,3 +150,33 @@ class SLL {
     return this;
   }
 }
+
+// TESTING:
+
+let l = new SLL();
+
+l.push(5).push(15).push(20).push(25);
+console.log('L with 4 nums: ', l); // 5,15,20,25, length 4
+console.log('insert 12 at 100idx: ', l.insert(100, 12)); // false
+console.log('insert 10 at first idx: ', l.insert(1, 10)); // true;
+console.log('insert 30 at tail: ', l.insert(5, 30)); // true;
+console.log('l with 30: ', l); // 5,10,15,20,25,30, length 6
+console.log('rotate -2: ', l.rotate(-2)); // 25,30,5,10,15,20, length 6
+console.log('get idx 3: ', l.get(3)); // 10
+console.log('get idx 2: ', l.get(2)); // 5
+console.log('set idx 0: ', l.set(0, 'HII')); // true (sets 25 to "HII")
+console.log('get idx 0: ', l.get(0)); // "HII";
+console.log('pop: ', l.pop()); // 20, next: null;
+console.log('l without 20: ', l); // "HII",30,5,10,15, length 5
+l.set(0, 25);
+l.push(20);
+console.log('l back to 6 items: ', l);
+console.log('rotate 2: ', l.rotate(2)); // 5,10,15,20,25,30
+console.log('remove tail: ', l.remove(5)); // should pop 30
+console.log('l without 30: ', l); // 5,10,15,20,25
+console.log('remove head: ', l.remove(0)); // 5
+console.log('L: ', l);
+console.log('remove tail: ', l.remove(3)); // 25
+console.log('remove 1000: ', l.remove(1000)); // undefined
+console.log('remove in middle: ', l.remove(1)); // 15
+console.log(l); // 10,20
